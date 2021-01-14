a2dadfc9c453d65ec0424da78128385fa1f49946
Avichai Levenson <avichailevenson@gmail.com>
2021-01-13 18:38:57 -0900

    initial commit

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # postgrest_test
 Demo setup with working docker-compose for postgrest
 
+## building the project
+docker-compose build 
+
 ## running the project
 docker-compose up
 
 ## remove containers
 docker-compose down
+*After running this, you have to build again -- needs fix*
 
 ## api runs on 
 http://localhost:3000
--- db.sql
+++ db.sql
@@ -1,6 +1,8 @@
 create schema api;
 
+
 CREATE EXTENSION pgcrypto;
+CREATE EXTENSION pgjwt;
 
 -- users
 create table api.users(
@@ -13,7 +15,29 @@ create table api.users(
   dob date not null
 );
 
-insert into api.users (first_name, last_name, password, email, sex, dob) 
+create table api.activities(
+  id serial primary key,
+  act_type varchar not null,
+  version varchar not null,
+  time int not null,
+  score int not null, 
+  avg_power int not null,
+  region varchar not null,
+  device varchar not null, 
+  act_rank varchar not null,
+  foreign key (id) references api.users(id) 
+); 
+
+create table api.purchases(
+  id serial primary key, 
+  purchase_date date not null, 
+  receipt_code varchar not null,
+  foreign key (id) references api.users(id) 
+); 
+
+
+/*
+insert into api.users (first_name, last_name, password, email, sex, dob)  
 	VALUES 
 		(
 			'jeff',
@@ -24,10 +48,58 @@ insert into api.users (first_name, last_name, password, email, sex, dob)
 			'1969-07-12'
 		);
 
+insert into api.activities (act_type, version, time, score, avg_power, region, device, act_rank)  
+	VALUES 
+		(
+			'biking',
+			1, 
+			30, 
+			100, 
+			24, 
+			'alaska', 
+      'iphone', 
+      'high'
+		);
+
+insert into api.purchases (purchase_date, receipt_code)
+VALUES (
+  '2021-01-11', 
+  'not sure'
+); 
+*/
+
+
+
+
+/* this block is from https://postgrest.org/en/v4.1/auth.html
+/  Commenting this block out makes the db run as expected. 
+/ 
+/  If left in the server starts, but fails to establish a connection because the 'web_anon' role 
+/  does not exist. 
+*/  
+
+create type jwt_token as (
+  token text
+);
+CREATE FUNCTION jwt_test() RETURNS public.jwt_token
+    LANGUAGE sql
+    AS $$ 'text'::text
+  SELECT sign(
+    row_to_json(r), 'mysecret'
+  ) AS token
+  FROM (
+    SELECT
+      'my_role'::text as role,
+      extract(epoch from now())::integer + 300 AS exp
+  ) r;
+$$;
+
+
 create role web_anon nologin;
 grant usage on schema api to web_anon;
 grant select on api.users to web_anon;
+grant select on api.activities to web_anon;
+grant select on api.purchases to web_anon;
 
 create role authenticator noinherit login password 'mysecretpassword';
 grant web_anon to authenticator;
-
